c1e8fda52bf06b520dab10d86cefe363fad23e36
kamiya10 <58339640+kamiya10@users.noreply.github.com>
2021-05-25 03:21:15 +0800

    調成15秒

--- command/admin/delmsg.js
+++ command/admin/delmsg.js
@@ -49,7 +49,7 @@ async function delmsg(message, args, client) {
                                 .setColor(client.colors.success)
                                 .setTitle(client.embedStat.success)
                                 .setDescription(`已成功刪除 \`${msgtodel.size}\` 則訊息 (機器人 \`${botcount}\` 則, 不包含釘選 \`${pincount}\` 則)`);
-                            const sent = await message.channel.send(embed).then(async m => setTimeout(async () => await m.delete(), 10000));
+                            await message.channel.send(embed).then(async m => setTimeout(async () => await m.delete(), 15000));
                             return;
                         });
                     })
