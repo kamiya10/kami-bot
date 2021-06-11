0555a32ab224192bfbcaf98384bc1386d1b50df9
kamiya10 <58339640+kamiya10@users.noreply.github.com>
2021-06-11 15:52:12 +0800

    更新

--- command/admin/purge.js
+++ command/admin/purge.js
@@ -19,42 +19,36 @@ async function purge(message, args, client) {
             .setTitle(client.embedStat.error)
 
 
-            
-        if (!args.length) {
-            error.setDescription("沒有提供數量");
+
+        if (!Number.isInteger(+args[0])) {
+            error.setDescription('數量必須是整數');
+            await message.reply(error);
+            return;
+        } else if (+args[0] > 100) {
+            error.setDescription('數量不能大於 `100`');
+            await message.reply(error);
+            return;
+        } else if (+args[0] < 1) {
+            error.setDescription('數量不能小於 `0`');
             await message.reply(error);
             return;
         } else {
-            if (!Number.isInteger(+args[0])) {
-                error.setDescription('數量必須是整數');
-                await message.reply(error);
-                return;
-            } else if (+args[0] > 100) {
-                error.setDescription('數量不能大於 `100`');
-                await message.reply(error);
-                return;
-            } else if (+args[0] < 1) {
-                error.setDescription('數量不能小於 `0`');
-                await message.reply(error);
-                return;
-            } else {
-                await message.delete();
-                await message.channel.messages.fetch({ limit: args[0] })
-                    .then(async messages => {
-                        const msgtodel = messages.filter(m => !m.pinned);
-                        const botcount = messages.filter(m => m.author.bot).size;
-                        const pincount = messages.filter(m => m.pinned).size;
-                        await message.channel.bulkDelete(msgtodel).then(async () => {
-                            const embed = new Discord.MessageEmbed()
-                                .setColor(client.colors.success)
-                                .setTitle(client.embedStat.success)
-                                .setDescription(`已成功刪除 \`${msgtodel.size}\` 則訊息 (機器人 \`${botcount}\` 則, 不包含釘選 \`${pincount}\` 則)`);
-                            await message.channel.send(embed).then(async m => setTimeout(async () => await m.delete(), 5000));
-                            return;
-                        });
-                    })
-                    .catch(console.error);
-            }
+            await message.delete();
+            await message.channel.messages.fetch({ limit: args[0] })
+                .then(async messages => {
+                    const msgtodel = messages.filter(m => !m.pinned);
+                    const botcount = messages.filter(m => m.author.bot).size;
+                    const pincount = messages.filter(m => m.pinned).size;
+                    await message.channel.bulkDelete(msgtodel).then(async () => {
+                        const embed = new Discord.MessageEmbed()
+                            .setColor(client.colors.success)
+                            .setTitle(client.embedStat.success)
+                            .setDescription(`已成功刪除 \`${msgtodel.size}\` 則訊息 (機器人 \`${botcount}\` 則, 不包含釘選 \`${pincount}\` 則)`);
+                        await message.channel.send(embed).then(async m => setTimeout(async () => await m.delete(), 5000));
+                        return;
+                    });
+                })
+                .catch(console.error);
         }
     } catch (e) {
         await message.reply(`發生了預料外的錯誤 \`${e.toString()}\``);
