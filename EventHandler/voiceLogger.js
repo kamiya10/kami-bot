1dadd94e1f62356293b3e896232f8ceefcc94d86
kamiya10 <58339640+kamiya10@users.noreply.github.com>
2022-12-20 20:22:42 +0800

    feat: voice logging

---
+++ EventHandler/voiceLogger.js
@@ -0,0 +1,61 @@
+const { ChannelType, Collection, Colors, EmbedBuilder } = require("discord.js");
+const logger = require("../Core/logger");
+
+module.exports = {
+  name  : "voiceCreate",
+  event : "voiceStateUpdate",
+  once  : false,
+
+  /**
+     *
+     * @param {import("discord.js").Client} client
+     * @param {import("discord.js").VoiceState} oldState
+     * @param {import("discord.js").VoiceState} newState
+     */
+  async execute(client, oldState, newState) {
+    try {
+      if (oldState.channelId != newState.channelId) {
+        const enter = [];
+        const leave = [];
+
+        if (oldState.channel && newState.channel) {
+          enter.push(
+            new EmbedBuilder()
+              .setDescription(`📥 ${newState.member} 加入了語音頻道（從${oldState.channel}）`)
+              .setColor(Colors.Green)
+              .setTimestamp(),
+          );
+          leave.push(
+            new EmbedBuilder()
+              .setDescription(`📤 ${oldState.member} 離開了語音頻道（到${newState.channel}）`)
+              .setColor(Colors.Red)
+              .setTimestamp(),
+          );
+        } else if (oldState.channel) {
+          leave.push(
+            new EmbedBuilder()
+              .setDescription(`📤 ${oldState.member} 離開了語音頻道`)
+              .setColor(Colors.Red)
+              .setTimestamp(),
+          );
+        } else if (newState.channel) {
+          enter.push(
+            new EmbedBuilder()
+              .setDescription(`📥 ${newState.member} 加入了語音頻道`)
+              .setColor(Colors.Green)
+              .setTimestamp(),
+          );
+        }
+
+        if (enter.length && newState.channel)
+          await newState.channel.send({ embeds: enter }).catch(() => void 0);
+
+        if (leave.length && oldState.channel)
+          await oldState.channel.send({ embeds: leave }).catch(() => void 0);
+      }
+    } catch (e) {
+      logger.error(`${this.name} has encountered an error: ${e} in ${oldState.guild.name} (${oldState.guild.id})`);
+      logger.error(e);
+    }
+  },
+};
