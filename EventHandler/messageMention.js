2d5700f0186f491af4880d052f698ec8633b4c87
Kamiya <58339640+kamiya10@users.noreply.github.com>
2023-02-25 13:13:19 +0800

    feat(MessageMention): add reply to message urls

---
+++ EventHandler/messageMention.js
@@ -0,0 +1,45 @@
+const { ChannelType, EmbedBuilder, time } = require("discord.js");
+
+module.exports = {
+  name  : "messageMention",
+  event : "messageCreate",
+  once  : false,
+
+  /**
+   * @param {import("discord.js").Client} client
+   * @param {import("discord.js").Message} message
+   */
+  async execute(client, message) {
+    // if (!client.database.GuildDatabase.get(message.guild.id).messageMention) return;
+
+    if (message.author.bot) return;
+
+    const match = message.content.match(/^https:\/\/discord\.com\/channels\/(\d+)\/(\d+)\/(\d+)$/);
+
+    if (match == null) return;
+
+    if (match[1] != message.guild.id) return;
+
+    /**
+     * @type {import("discord.js").Message<true>}
+     */
+    const mentioned = await message.guild.channels.cache.get(match[2]).messages.fetch({ message: match[3] }).catch(() => void 0);
+
+    if (mentioned == null || mentioned.partial) return;
+
+    if (mentioned.content.length) {
+
+      if (mentioned.member == null)
+        await mentioned.guild.members.fetch({ user: mentioned.author.id });
+
+      const embed = new EmbedBuilder()
+        .setColor(mentioned.member.displayHexColor)
+        .setAuthor({ name: mentioned.member.displayName, iconURL: mentioned.member.displayAvatarURL() })
+        .setDescription(mentioned.content)
+        .setTimestamp(mentioned.createdAt);
+      await message.reply({ content: `${mentioned.author} ${time(mentioned.createdAt, "F")}, 在 ${mentioned.channel}, 在 ${mentioned.guild}`, embeds: [embed, ...mentioned.embeds], allowedMentions: { parse: [] } });
+    } else if (mentioned.embeds.length) {
+      await message.reply({ content: `${mentioned.author} ${time(mentioned.createdAt, "F")}, 在 ${mentioned.channel}, 在 ${mentioned.guild}`, embeds: mentioned.embeds, allowedMentions: { parse: [] } });
+    }
+  },
+};
