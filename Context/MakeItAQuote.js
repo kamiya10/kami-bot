287f1d20754722a3d5493e7547abb813eca8a32f
Kamiya <58339640+kamiya10@users.noreply.github.com>
2023-05-13 19:03:43 +0800

    fix: referencing database

--- Context/MakeItAQuote.js
+++ Context/MakeItAQuote.js
@@ -16,7 +16,7 @@ module.exports = {
   async execute(interaction) {
     const embed = new EmbedBuilder();
 
-    if ((interaction.database.UserDatabase.get(interaction.targetMessage.author.id)?.allow_quote ?? true) != true) {
+    if ((interaction.client.database.UserDatabase.get(interaction.targetMessage.author.id)?.allow_quote ?? true) != true) {
       embed
         .setColor(Colors.Red)
         .setDescription("❌ 這個訊息的作者不允許任何人把它變成名言");
