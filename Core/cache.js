74ab9a520594ce9c91846db7ca29d986410320de
Kamiya <58339640+kamiya10@users.noreply.github.com>
2023-07-31 17:21:19 +0800

    feat: cache manager (maybe?

---
+++ Core/cache.js
@@ -0,0 +1,45 @@
+const { existsSync, mkdirSync, readFileSync, writeFileSync } = require("node:fs");
+const { join } = require("node:path");
+
+const cacheFolder = join(__dirname, "../.cache");
+const cacheFile = join(cacheFolder, "cache.json");
+let cachedData = {};
+
+const cache = (key, data, expire) => {
+  if (!existsSync(cacheFolder))
+    mkdirSync(cacheFolder);
+
+  cachedData[key] = {
+    cachedTimestamp  : Date.now(),
+    expiresTimestamp : expire,
+    data,
+  };
+
+  writeFileSync(cacheFile, JSON.stringify(cachedData));
+};
+
+const checkExpires = (timestamp) => timestamp && Date.now() > timestamp;
+
+cache.get = (key) => {
+  if (cachedData[key])
+    if (checkExpires(cachedData[key].expiresTimestamp))
+      delete cachedData[key];
+
+  writeFileSync(cacheFile, JSON.stringify(cachedData));
+  return cachedData[key]?.data;
+};
+
+cache.delete = (key) => {
+  delete cachedData[key];
+};
+
+if (existsSync(cacheFile)) {
+  cachedData = JSON.parse(readFileSync(cacheFile, { encoding: "utf-8" }));
+  for (const key in cachedData)
+    if (checkExpires(cachedData[key].expiresTimestamp))
+      delete cachedData[key];
+
+  writeFileSync(cacheFile, JSON.stringify(cachedData));
+}
+
+module.exports = { cache };
