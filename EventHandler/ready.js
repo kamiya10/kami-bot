b33afcf0d2e82da1e866b14313172d485252c271
Kamiya <58339640+kamiya10@users.noreply.github.com>
2024-07-22 17:16:06 +0800

    fix: dont delete channels now

--- EventHandler/ready.js
+++ EventHandler/ready.js
@@ -13,6 +13,8 @@ module.exports = {
     await client.guilds.fetch({ force: true });
 
     logger.info("The bot is online");
+
+    /*
     const GuildSetting = await client.database.GuildDatabase.getAll(["voice"]);
     const checklist = Object.keys(GuildSetting).filter(v => GuildSetting[v]?.voice?.length).map(v => GuildSetting[v]);
     checklist.forEach(v => {
@@ -21,9 +23,6 @@ module.exports = {
           if (client.channels.cache.get(val.category))
             client.channels.cache.get(val.category).children.cache.forEach(
 
-              /**
-               * @param {import("discord.js").Channel} ch
-               */
               async ch => {
                 if (ch.isVoiceBased())
                   if (ch.id != val.creator)
@@ -31,11 +30,13 @@ module.exports = {
                       const m = ch.permissionOverwrites.cache.filter((p, k) => p.allow.has(PermissionFlagsBits.ManageChannels) && k != client.user.id);
                       client.watchedChanels.set(ch.id, { master: m?.firstKey() });
                     } else {
-                      await ch.delete();
+                      await ch.delete().catch(() => void 0);
                     }
               });
       });
     });
+    */
+
     setInterval(async () => {
       await client.user.setActivity(`${client.version} | ${client.guilds.cache.size}伺服 - ${client.channels.cache.size}頻道 - ${client.users.cache.size}用戶`);
     }, 60000);
