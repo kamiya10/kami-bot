77ed2a19faaa5bd7c8fb2f86861fc92653f2d07f
Kamiya <kamiya@kamiya.app>
2024-08-03 17:56:43 +0800

    fix: temporary fix for tra 403

--- Core/client.js
+++ Core/client.js
@@ -13,21 +13,28 @@ const KamiIntents = [
   GatewayIntentBits.GuildVoiceStates,
 ];
 
-const Kami = new Client({ intents: KamiIntents, allowedMentions: { parse: ["roles", "everyone"] } });
+const Kami = new Client({
+  intents: KamiIntents,
+  allowedMentions: { parse: ["roles", "everyone"] },
+});
 
-Kami.database = { GuildDatabase: new KamiDatabase("guild.json"), UserDatabase: new KamiDatabase("user.json") };
+Kami.database = {
+  GuildDatabase: new KamiDatabase("guild.json"),
+  UserDatabase: new KamiDatabase("user.json"),
+};
 
 // #region Event Registion
 
-const eventFiles = fs.readdirSync("./EventHandler").filter(file => file.endsWith(".js"));
+const eventFiles = fs
+  .readdirSync("./EventHandler")
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of eventFiles) {
   const event = require(path.resolve(`./EventHandler/${file}`));
 
   if (event.once)
     Kami.once(event.event, (...args) => event.execute(Kami, ...args));
-  else
-    Kami.on(event.event, (...args) => event.execute(Kami, ...args));
+  else Kami.on(event.event, (...args) => event.execute(Kami, ...args));
 }
 
 // #endregion
@@ -38,7 +45,9 @@ Kami.commands = new Collection();
 const commandCategories = fs.readdirSync("./Commands");
 
 for (const category of commandCategories) {
-  const commandFiles = fs.readdirSync(`./Commands/${category}`).filter(file => file.endsWith(".js"));
+  const commandFiles = fs
+    .readdirSync(`./Commands/${category}`)
+    .filter((file) => file.endsWith(".js"));
 
   for (const file of commandFiles) {
     const command = require(path.resolve(`./Commands/${category}/${file}`));
@@ -47,7 +56,9 @@ for (const category of commandCategories) {
 }
 
 Kami.context = new Collection();
-const commandFiles = fs.readdirSync("./Context").filter(file => file.endsWith(".js"));
+const commandFiles = fs
+  .readdirSync("./Context")
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
   const command = require(path.resolve(`./Context/${file}`));
@@ -60,8 +71,12 @@ for (const file of commandFiles) {
 
 Kami.locale = {};
 
-for (const locale of fs.readdirSync("./Localization").filter(file => file.endsWith(".json")))
-  Kami.locale[locale.slice(0, -5)] = require(path.resolve(`./Localization/${locale}`));
+for (const locale of fs
+  .readdirSync("./Localization")
+  .filter((file) => file.endsWith(".json")))
+  Kami.locale[locale.slice(0, -5)] = require(path.resolve(
+    `./Localization/${locale}`
+  ));
 
 // #endregion
 
@@ -69,17 +84,17 @@ Kami.cooldowns = new Collection();
 Kami.watchedChanels = new Collection();
 Kami.forecast = new Collection();
 Kami.data = {
-  quake_data   : [],
-  quake_data_s : [],
-  quake_last   : [],
-  quake_last_s : [],
-  quake_image  : new Collection(),
-  rts_stations : new Collection(),
-  rts_list     : new Collection(),
+  quake_data: [],
+  quake_data_s: [],
+  quake_last: [],
+  quake_last_s: [],
+  quake_image: new Collection(),
+  rts_stations: new Collection(),
+  rts_list: new Collection(),
 };
 
 // Kami.eqws - require("./websocket")(Kami, 5000);
-require("../API/tdx").TDX.init(process.env.TDX_ID, process.env.TDX_SECRET).then(tdx => {
+/* require("../API/tdx").TDX.init(process.env.TDX_ID, process.env.TDX_SECRET).then(tdx => {
   Kami.tdx = tdx;
 
   const liveboard = () => {
@@ -94,6 +109,6 @@ require("../API/tdx").TDX.init(process.env.TDX_ID, process.env.TDX_SECRET).then(
 
   liveboard();
   setInterval(liveboard, 60000);
-});
+}); */
 
 module.exports = { Kami };
