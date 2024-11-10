f2d9d8d756ebec2e479354bd2c91ec02cfaf0797
Kamiya <kamiya@kamiya.app>
2024-11-10 10:13:16 +0800

    feat: client activity

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -1,14 +1,25 @@
 import { EventHandler } from '@/class/event';
 
 import logger from 'logger';
+import pkg from '~/package.json';
 
 export default new EventHandler({
   event: 'ready',
   async on(client) {
     await this.updateCommands();
     this.sweepStates();
+
     logger.info(
       `Client is ready as ${client.user.tag} with ${client.guilds.cache.size} guild${client.guilds.cache.size > 1 ? 's' : ''}.`,
     );
+
+    const updateStatus = () => {
+      client.user.setActivity({
+        name: `v${pkg.version} | ${client.guilds.cache.size} 伺服器`,
+      });
+    };
+
+    updateStatus();
+    setInterval(updateStatus, 600_000);
   },
 });
