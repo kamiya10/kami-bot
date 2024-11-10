49fa92255dc650b03caeb7127e6b79e6acc63d31
Kamiya <kamiya@kamiya.app>
2024-11-10 10:11:38 +0800

    fix: bundler

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,26 +3,26 @@ import json from '@rollup/plugin-json';
 import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 
-import { resolve } from 'path';
-
 export default {
   input: 'src/index.ts',
   output: {
-    dir: 'dist',
+    file: "dist/index.js",
     format: 'es',
   },
   plugins: [
-    alias({
-      entries: [
-        { find: 'logger', replacement: resolve('src', 'class', 'logger') },
-        { find: '$', replacement: resolve('src', 'commands') },
-        { find: '#', replacement: resolve('src', 'events') },
-        { find: '@', replacement: resolve('src') },
-        { find: '~', replacement: resolve() },
-      ],
-    }),
     json(),
     terser(),
-    typescript(),
+    typescript({
+      tsconfig: './tsconfig.json',
+    }),
+    alias({
+      entries: [
+        { find: 'logger', replacement: './src/class/logger' },
+        { find: '$', replacement: './src/commands' },
+        { find: '#', replacement: './src/events' },
+        { find: '@', replacement: './src' },
+        { find: '~', replacement: '.' },
+      ],
+    }),
   ],
 };
